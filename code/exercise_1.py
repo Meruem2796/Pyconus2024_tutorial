3fc97ef083ca3c0715954fb5c8260a9c0bbedeef
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-02-18 03:26:34 +0100

    Exercise_1 code modified

--- code/exercise_1.py
+++ code/exercise_1.py
@@ -8,21 +8,8 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         # 1. Get the list of quotes availabe at the page
-        for quote in response.css(".quote"):
-            yield {
-                "quote": quote.css("span.text::text").get(),
-                "author": quote.css("small::text").get(),
-                "url": response.urljoin(quote.css("span a::attr(href)")).get(),
-                "tags": quote.css(".tags a::text").getall(),
-                "urls_tag": response.urljoin(
-                    quote.css(".tags a::attr(href)")
-                ).getall(),
-            }
 
         # 2. Parse each quote found and yield the quote item
 
         # 3. Follow the next page link
-        yield scrapy.Request(
-            response.urljoin(response.css(".next a::attr(href)").get())
-        )
         ...
